b6097ec66bcde828cae3eabfe22c5504f65a6dab
Bram <Bram_vos@hotmail.com>
2020-03-18 23:34:30 +0100

    Fixed the service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = "cache2";
+const cacheName = "cache1";
 const appFiles = [
     "/manifest.json",
     "/js/scripts.js",
@@ -78,31 +78,3 @@ self.addEventListener("push", (pushing) => {
 })
 
 
-
-//Asking for permission with the Notification API
-if (typeof Notification !== typeof undefined) { //First check if the API is available in the browser
-    Notification.requestPermission().then(function (result) {
-        //If accepted, then save subscriberinfo in database
-        if (result === "granted") {
-            console.log("Browser: User accepted receiving notifications, save as subscriber data!");
-            navigator.serviceWorker.ready.then(function (serviceworker) { //When the Service Worker is ready, generate the subscription with our Serice Worker's pushManager and save it to our list
-                const VAPIDPublicKey = "BPKwxO6GiIh3enjXrV5yNf3g34HImk9tM9rYKogNXwlv-Ae-x5pfOyfC0afE9X60pAnK6LLy0WGsCaZ8fULsWIY"; // Fill in your VAPID publicKey here
-                const options = {
-                    applicationServerKey: VAPIDPublicKey,
-                    userVisibleOnly: true
-                } //Option userVisibleOnly is neccesary for Chrome
-                serviceworker.pushManager.subscribe(options).then((subscription) => {
-                    //POST the generated subscription to our saving script (this needs to happen server-side, (client-side) JavaScript can't write files or databases)
-                    let subscriberFormData = new FormData();
-                    subscriberFormData.append("json", JSON.stringify(subscription));
-                    fetch("data/saveSubscription.php", {
-                        method: "POST",
-                        body: subscriberFormData
-                    });
-                });
-            });
-        }
-    }).catch((error) => {
-        console.log(error);
-    });
-}
